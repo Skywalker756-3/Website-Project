f7e1d6f5e1658b1d90b5350ff6a59a856b85bd2d
= <johndoe@example.com>
2025-02-19 15:12:01 -0500

    Fixed the menu position.

--- script.js
+++ script.js
@@ -2,18 +2,15 @@ let slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
-function plusSlides(n) {
+function incrementSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+
 
 function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
+  let slides = document.getElementsByClassName("slide");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
@@ -22,7 +19,7 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
 } 
 
-//Prevent the site from being visible before elements are loaded in
+//Prevent the site from being visible before elements are loaded in, this uses an event
 let domReady = (cb) => {
     document.readyState === 'interactive' || document.readyState === 'complete'
       ? cb()
